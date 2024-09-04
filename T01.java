3e619965783c6bc15e65fe6d65114837545dffa6
YesikaSaragih <saragihyesika@gmail.com>
2024-09-04 21:25:53 +0700

    kami sudah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24024 - Yesika Nadia Saragih
+// 12S24024 - Yesika Nadia Saragih
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
 
